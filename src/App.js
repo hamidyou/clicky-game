edaa30b65fb157e3328621c6bf88a43db02a439a
Youssef <yhamid1982@gmail.com>
2018-12-10 22:16:19 -0500

    moved images to public

--- src/App.js
+++ src/App.js
@@ -3,15 +3,50 @@ import './App.css'
 import Header from './Header/Header'
 import Scoreboard from './Scoreboard/Scoreboard'
 import Cards from './Cards/Cards'
+import movies from './images/images.json'
+import { concat, includes } from 'kyanite/dist/kyanite'
 
-const App = () => {
-  return (
-    <div className='App'>
-      <Header />
-      <Scoreboard />
-      <Cards />
-    </div>
-  )
+class App extends Component {
+  state = {
+    selected: [],
+    currentScore: "0",
+    highScore: "2"
+  }
+
+  checkScore = score => score > this.state.highScore ? this.setState({ highScore: score }) : null
+
+  selectMovie = id => {
+    const selectedMovies = this.state.selected
+    if (includes(id, selectedMovies)) {
+      return alert('Game over....you lose!')
+    } else {
+      let score = this.state.currentScore
+      this.setState({
+        currentScore: score++
+      })
+      this.checkScore(this.state.currentScore)
+      this.setState({
+        selected: concat(id, selectedMovies)
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className='App' >
+        <Header />
+        <Scoreboard />
+        {movies.map(movie => (
+          <Cards
+            selectMovie={this.selectMovie}
+            id={movie.id}
+            key={movie.id}
+            image={movie.src}
+          />))}
+      </div>
+    )
+  }
+
 }
 
 export default App
--- src/Cards/Cards.js
+++ src/Cards/Cards.js
@@ -1,34 +1,12 @@
 import React, { Component } from 'react'
 import { Card, CardImg, Row, Col, Container } from 'reactstrap'
 
-class Cards extends Component {
-  state = {
-    clicked: false
-  }
-
-  imageClicked = event => {
-    event.preventDefault();
-    if (this.state.clicked) {
-      return alert('Sorry this one was already selected.')
-    } else {
-      this.setState({
-        clicked: true
-      })
-      return alert('Nice job')
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <Container>
-          <Card>
-            <CardImg top max-width="200px" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-          </Card>
-        </Container>
-      </div>
-    )
-  }
-}
+const Cards = props => (
+  <div>
+    <Card onClick={props.selectMovie} id={props.id}>
+      <CardImg src={props.image} />
+    </Card>
+  </div>
+)
 
 export default Cards
