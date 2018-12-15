eb58c671c7b17fc9d4c3dc82d2aeeb5a1c9591ed
Youssef <yhamid1982@gmail.com>
2018-12-14 23:12:56 -0500

    Updates

---
+++ precache-manifest.d54b3206bdf4c3ed12ff2995ebf1edc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "15916205af30d6f5eb9d7898d6cff3ab",
+    "url": "/clicky-game/static/media/disney-hero.15916205.jpg"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "106a3255de94807f3708",
+    "url": "/clicky-game/static/js/main.106a3255.chunk.js"
+  },
+  {
+    "revision": "3ca83a3da716f2c479d1",
+    "url": "/clicky-game/static/js/1.3ca83a3d.chunk.js"
+  },
+  {
+    "revision": "106a3255de94807f3708",
+    "url": "/clicky-game/static/css/main.53532577.chunk.css"
+  },
+  {
+    "revision": "3ca83a3da716f2c479d1",
+    "url": "/clicky-game/static/css/1.f27cb5d1.chunk.css"
+  },
+  {
+    "revision": "987fc54b2cb45a4ddbebe58637bbc8f9",
+    "url": "/clicky-game/index.html"
+  }
+];
